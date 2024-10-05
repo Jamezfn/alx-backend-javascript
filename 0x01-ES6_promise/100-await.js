30b4e68a2a8b580a38e60ff060934484d3871508
Jamezfn <jamesmakachia19@gmail.com>
2024-10-05 01:37:08 -0700

    eslint

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils.js';
 
 export default async function asyncUploadUser() {
-    try {
-        const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
-        return { photo, user };
-    } catch (error) {
-        return { photo: null, user: null };
-    }
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    return { photo, user };
+  } catch (error) {
+    return { photo: null, user: null };
+  }
 }
 
--- 0x01-ES6_promise/eslint.config.mjs
+++ 0x01-ES6_promise/eslint.config.mjs
@@ -1,6 +1,24 @@
-import globals from "globals";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        // Add any additional globals here if needed
+      },
+    },
+  },
+  {
+    ...pluginJs.configs.recommended,
+    rules: {
+      // Your custom rules here
+      'indent': ['error', 2], // Enforce 2-space indentation
+      'quotes': ['error', 'single'], // Enforce single quotes
+      'semi': ['error', 'always'], // Require semicolons
+      // Add any other specific rules you want to enforce
+    },
+  },
 ];
+
